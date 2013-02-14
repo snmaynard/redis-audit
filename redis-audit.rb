7ffb7b91c99f083cc3e94d415a30071294d839b8
Andrew Glover <aglover@beacon50.com>
2013-02-14 16:42:30 -0500

    added bundler

--- redis-audit.rb
+++ redis-audit.rb
@@ -20,7 +20,7 @@
 #    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 #    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-require 'rubygems'
+require 'bundler/setup'
 require 'redis'
 
 # Container class for stats around a key group
